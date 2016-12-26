826a12485ce6cb62d3e088f4ad2b745639648479
Zack Holloway <ZackHolloway@Zacks-MBP.home>
2016-12-26 18:22:09 -0500

    test commit

--- application/views/login.php
+++ application/views/login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <hmtl>
   <body>
-      <p class="testclass">Please log in. The default username is "Super". The default password is "Mario".</p>
-      <form method="post" action="site/userprofile">
+      <p>Please log in. The default username is "Super". The default password is "Mario".</p>
+      <form method="post" action="index.php/site/userprofile">
         <label>Username: </label> <input type="text" name="username"><br>
         <label>Password: </label> <input type="password" name="password"><br>
         <input type="submit" name="submit" value="Log In"><br>
